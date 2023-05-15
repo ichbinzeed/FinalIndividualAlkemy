58413d0d3ab0134e21c97a056981ce2c897c4f53
ichbinzeed <prog.gustavo@gmail.com>
2023-05-15 16:14:28 -0300

    cambios añadidos

--- StockControl/compra/admin.py
+++ StockControl/compra/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "apellido", "dni")
+    search_fields = ("apellido", "dni")
+    list_filter = ("apellido", "dni")
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "precio", "stock_actual")
+    search_fields = ("nombre", "precio", "stock_actual")
+    list_filter = ("nombre", "precio", "stock_actual")
+
+admin.site.register(models.Proveedor, ProveedorAdmin)
+admin.site.register(models.Producto, ProductoAdmin)
---
+++ StockControl/readme.md
@@ -0,0 +1,14 @@
+#Proyecto Integrador Individual
+
+Poner en marcha el proyecto:
+-Crea el entorno virtual : python -m venv "nombre-entorno-vir"
+
+-Activar el entorno:  nombre-entorno\Scripts\activate
+
+-Instalar dependencias : pip install -r requirements.txt
+
+-Ejecutar el siguiente comando : python manage.py makemigrations
+-Ejecutar el siguiente comando : python manage.py migrate
+-Ejecutar el siguiente comando : python manage.py runserver
+
+-Abrir el localhost:8000
